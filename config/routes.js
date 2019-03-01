129845f330f2e025465c0a0d418d9372d00d4935
Randy Wilson <phoenixrr2113@gmail.com>
2019-03-01 12:20:09 -0500

    I got carried away. forgot to commit

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,13 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require('../middleware/auth/authenticate');
+const generateToken = require('../middleware/tokenServices');
+const Users = require('../users/userModel');
+const usersRouter = require('../users/usersRouter');
 
 module.exports = server => {
+	server.get('/users', authenticate, usersRouter);
 	server.post('/api/register', register);
 	server.post('/api/login', login);
 	server.get('/api/jokes', authenticate, getJokes);
@@ -10,10 +15,35 @@ module.exports = server => {
 
 function register(req, res) {
 	// implement user registrations
+	let { username, password } = req.body;
+	const hash = bcrypt.hashSync(password, 10);
+	password = hash;
+
+	Users.add({ username, password })
+		.then(newUser => {
+			res.status(200).json(newUser);
+		})
+		.catch(err => res.status(500).json(err));
 }
 
 function login(req, res) {
 	// implement user login
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user);
+				res.status(200).json({
+					message: `Welcome ${user.username}!`,
+					token,
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid creds' });
+			}
+		})
+		.catch(err => res.status(500).json(err));
 }
 
 function getJokes(req, res) {
